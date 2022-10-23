27095a092e4ad1be85a7525e959d3888ff2c401a
sejan mahmud <sejan157218@gmail.com>
2022-10-24 02:20:53 +0600

    118. Pascal's Triangle

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,26 @@
+from unittest import result
+
+
+class Solution:
+    def generate(self, numRows: int):
+        result=[[1]]
+        if numRows==1:
+            return result
+        i=0
+        while i<numRows-1:
+            if i<(len(result)):
+                result.append(self.SumOfArray(result[i]))
+            i+=1
+        return result
+    def SumOfArray(self,arr):   
+        res=[]
+        for j in range(len(arr)-1):
+            res.append(arr[j]+arr[j+1])
+        return  [1]+res+[1]
+
+  
+
+s=Solution()
+numRows = 5
+
+print(s.generate(numRows))
