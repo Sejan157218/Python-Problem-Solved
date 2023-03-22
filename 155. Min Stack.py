b8ae2261593325f9ab87c294063b56874fdfea0f
sejan mahmud <sejan157218@gmail.com>
2023-03-23 00:42:51 +0600

    155. Min Stack

---
+++ 155. Min Stack.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.array=[]
+        
+
+    def push(self, val: int) -> None:
+        return self.array.append(val)
+
+    def pop(self) -> None:
+        return self.array.pop()
+        
+
+    def top(self) -> int:
+        return self.array[-1]
+        
+
+    def getMin(self) -> int:
+     
+        return min(self.array)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
